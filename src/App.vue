759dd5210330ec3fdef479a37664979c927db070
Charles <uyo@hotmail.fr>
2019-10-23 22:58:36 +0200

    First version with canvas component

--- src/App.vue
+++ src/App.vue
@@ -30,28 +30,7 @@
       <v-container class="fill-height" fluid>
         <v-row align="center" justify="center">
           <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn :href="source" icon large target="_blank" v-on="on">
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  icon
-                  large
-                  href="https://codepen.io/johnjleider/pen/bXNzZL"
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-codepen</v-icon>
-                </v-btn>
-              </template>
-              <span>Codepen</span>
-            </v-tooltip>
+            <GameCanvas/>
           </v-col>
         </v-row>
       </v-container>
@@ -64,6 +43,8 @@
 </template>
 
 <script>
+import GameCanvas from "./components/GameCanvas"
+
 export default {
   props: {
     source: String
@@ -73,6 +54,9 @@ export default {
   }),
   created () {
     this.$vuetify.theme.dark = true
+  },
+  components: {
+    GameCanvas
   }
 }
 </script>
---
+++ src/components/GameCanvas.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>
+            Mon titre
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "GameCanvas"
+}
+</script>
